ccafe922c8d93cc608cb0929dc105c23222d9dcf
kdsabella <kensabella04@gmail.com>
2022-06-06 09:08:42 +0800

    calculator 1st commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+// console.log(buttons)
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case 'Del':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1)
+                }
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Error'
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+
+        }
+    })
+})
